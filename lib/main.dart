e61b45edd79f31fa936368b885f71ec501373c22
tylercrews <tycrews64@gmail.com>
2026-02-21 20:31:15 -0500

    got a ship and bullets shooting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:flame/components.dart';
 import 'package:flutter/services.dart';
+// import 'dart:math' as math;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -43,26 +45,175 @@ class GameScreen extends StatelessWidget {
 }
 
 class BlockBlasterGame extends FlameGame {
+  late PlayerShip player;
+  late List<Shot> shots;
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    // Game initialization code goes here
+    shots = [];
+    
+    debugPrint('Game size: ${size.x} x ${size.y}');
+    
+    // Create the player ship in the center-bottom of the screen
+    player = PlayerShip(
+      gameRef: this,
+    );
+    player.position = Vector2(size.x / 2, size.y - 100);
+    add(player);
+    
     debugPrint('BlockBlasterGame loaded!');
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    // Game update logic goes here
+    
+    // Remove off-screen shots
+    shots.removeWhere((shot) {
+      if (shot.position.y < 0) {
+        remove(shot);
+        return true;
+      }
+      return false;
+    });
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color(0xFF1a1a2e);
+  }
+
+  void addShot(Shot shot) {
+    shots.add(shot);
+    add(shot);
+  }
+}
+
+class PlayerShip extends PositionComponent {
+  final BlockBlasterGame gameRef;
+  double shootTimer = 0;
+  final double shootInterval = 1 / 6; // 6 shots per second
+  static const double shipWidth = 60;
+  static const double shipHeight = 50;
+
+  PlayerShip({
+    required this.gameRef,
+  });
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    size = Vector2(shipWidth, shipHeight);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    
+    // Update shoot timer
+    shootTimer += dt;
+    
+    // Shoot at 6 per second
+    if (shootTimer >= shootInterval) {
+      _shoot();
+      shootTimer = 0;
+    }
+  }
+
+  void _shoot() {
+    final shot = Shot(
+      gameRef: gameRef,
+    );
+    shot.position = Vector2(position.x, position.y - shipHeight / 2);
+    gameRef.addShot(shot);
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    // Render background
+    
+    final paint = Paint()..color = Colors.blue;
+    final outlinePaint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2;
+
+    // Draw left half of ship
     canvas.drawRect(
-      Rect.fromLTWH(0, 0, size.x, size.y),
-      Paint()..color = const Color(0xFF1a1a2e),
+      Rect.fromLTWH(
+        -shipWidth / 2,
+        -shipHeight / 2,
+        shipWidth / 2,
+        shipHeight,
+      ),
+      paint,
+    );
+    canvas.drawRect(
+      Rect.fromLTWH(
+        -shipWidth / 2,
+        -shipHeight / 2,
+        shipWidth / 2,
+        shipHeight,
+      ),
+      outlinePaint,
+    );
+
+    // Draw right half of ship
+    canvas.drawRect(
+      Rect.fromLTWH(
+        0,
+        -shipHeight / 2,
+        shipWidth / 2,
+        shipHeight,
+      ),
+      paint,
+    );
+    canvas.drawRect(
+      Rect.fromLTWH(
+        0,
+        -shipHeight / 2,
+        shipWidth / 2,
+        shipHeight,
+      ),
+      outlinePaint,
+    );
+  }
+}
+
+class Shot extends PositionComponent {
+  final BlockBlasterGame gameRef;
+  static const double shotRadius = 8;
+  static const double shotSpeed = 400; // pixels per second
+
+  Shot({
+    required this.gameRef,
+  });
+
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    size = Vector2(shotRadius * 2, shotRadius * 2);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    // Move shot upward
+    position.y -= shotSpeed * dt;
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    
+    final paint = Paint()..color = Colors.yellow;
+    
+    // Draw shot as a circle
+    canvas.drawCircle(
+      Offset.zero,
+      shotRadius,
+      paint,
     );
   }
 }
